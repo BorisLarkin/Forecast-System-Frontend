0367273118c30cfa4974ba9485744737b25d5253
BorisLarkin <borislarkin18@mail.ru>
2025-01-07 18:26:13 +0300

    clean

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-import ContextWrapper from "./components/ContextWrapper/ContextWrapper.tsx";
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ContextWrapper/>
-)
