249470028f8fec71bb445c4b04e18cf5bdcb5edd
BorisLarkin <borislarkin18@mail.ru>
2025-01-30 14:04:37 +0300

    tauri works

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import MainPage from "./pages/MainPage/MainPage"
 import ForecastsPage from "./pages/ForecastsPage/ForecastsPage"
 import ForecastPage from "./pages/ForecastPage/ForecastPage";
 import { ROUTES } from "./Routes";
-
+import { useEffect } from 'react';
+import { invoke } from "@tauri-apps/api/core";
 
 function App() {
+    useEffect(()=>{
+        invoke('tauri', {cmd: 'create'})
+            .then((response: any) => console.log(response))
+            .catch((error: any) => console.log(error));
+
+        return() => {
+            invoke('tauri', {cmd: 'close'})
+                .then((response: any) => console.log(response))
+                .catch((error: any) => console.log(error));
+        }
+    }, []);
+
     return (
         <BrowserRouter basename='/Forecast-System-Frontend'>
             <Routes>
